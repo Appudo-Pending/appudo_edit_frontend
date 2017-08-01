35095ebcabb026b50d0f7f0bea6abaa591dc21d6
pending@appudo.com <neon@neonlap>
2017-08-01 21:43:13 +0200

    Initial commit

---
+++ edit/e/nls/en/editor.js
@@ -0,0 +1,21 @@
+/*
+    editor.js is part of Appudo
+
+    Copyright (C) 2012
+        5b48f015bed78115b4cad80b91d67dd8690ba73b415a5c126d73493b540c6397 source@appudo.com
+
+    Licensed under the Apache License, Version 2.0
+
+    See http://www.apache.org/licenses/LICENSE-2.0 for more information
+*/
+
+define(
+//begin v1.x content
+({
+	txt:'Text Mode',
+	html:'HTML Mode',
+	css:'CSS Mode',
+	js:'Javascript Mode'
+})
+//end v1.x content
+);
